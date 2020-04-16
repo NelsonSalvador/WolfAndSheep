5bb0332c30164f19d2f71001759b5a06042fae1b
Pedro-Coutinho <pedro_coutinho18@outlook.com>
2020-04-16 22:26:47 +0100

    Melhorias de tabuleiro e inicialização do lobo

--- Program.cs
+++ Program.cs
@@ -14,25 +14,44 @@ namespace WolfAndSheep
 
             Instructions();
 
-            BoardInit(out board);
+            
 
             // Por as ovelas "X" nas suas posições iniciais
+            for (int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    board[i, j] = " ";
+                }
+            }
             int X1x = 7, X1y = 0;
             int X2x = 7, X2y = 2;
             int X3x = 7, X3y = 4;
             int X4x = 7, X4y = 6;
-            board[X1x,X1y] = "|X1|";
-            board[X2x,X2y] = "|X2|";
-            board[X3x,X3y] = "|X3|";
-            board[X4x,X4y] = "|X4|";
+            board[7,X1y] = "S";
+            board[7,X2y] = "S";
+            board[7,X3y] = "S";
+            board[7,X4y] = "S";
 
-            board[0, 1] = "|1_|";
-            board[0, 3] = "|2_|";
-            board[0, 5] = "|3_|";
-            board[0, 7] = "|4_|";
-
+            board[0, 1] = "1";
+            board[0, 3] = "2";
+            board[0, 5] = "3";
+            board[0, 7] = "4";
             PrintBoard(board);
-            WolfInit(out board);
+            int wolfPlace = WolfInit();
+            board[0, wolfPlace] = "W";
+
+            for (int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    if ( board[i, j] != "W" && board[i, j] != "S" )
+                    {
+                        board[i, j] = " ";
+                    }
+                }
+            }
+
 
             // Exemplo de move forwar de uma ovelha 
             while (playerinput != "exit")
@@ -105,58 +124,72 @@ namespace WolfAndSheep
         }
 
         //Input dos quadrados do tabuleiro no array multidimensional 
-        private static void BoardInit(out string[,] array)
+        /*private static void BoardInit(out string[,] array)
         {
             array = new string[8,8];
             for (int i = 0; i < 8; i++)
             {
                 for(int j = 0; j < 8; j++)
                 {
-                    array[i,j] = "|__|";
+                    if( j == 7)
+                    {
+                        Console.Write("| {0} |", array[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("| {0} ", array[i, j]);
+                    }
                 }
+                Console.WriteLine("");
             }
-        }
+        }*/
 
         // Output do tabuleiro
         private static void PrintBoard(string[,] board)
         {
             for (int i = 0; i < 8; i++)
             {
+                Console.WriteLine(" --- --- --- --- --- --- --- ---");
                 for(int j = 0; j < 8; j++)
                 {
-                    Console.Write("{0}", board[i,j]);
+                    if( j == 7)
+                    {
+                        Console.Write("| {0} |", board[i,j]);
+                    }
+                    else
+                    {
+                        Console.Write("| {0} ", board[i,j]);
+                    }
+                    
                 }
                 Console.WriteLine("");
+                if ( i == 7)
+                {
+                   Console.WriteLine(" --- --- --- --- --- --- --- ---"); 
+                }
             }
         }
 
-        private static void WolfInit(out string[,] array)
+        private static int WolfInit()
         {
-            array = new string[8,8];
-            BoardInit(out array);
+            //BoardInit(out array);
             Console.WriteLine("Wolf Start House (1, 2, 3 or 4): ");
             int Start = Convert.ToInt32(Console.ReadLine());
 
-            array[7, 0] = "|X1|";
-            array[7, 2] = "|X2|";
-            array[7, 4] = "|X3|";
-            array[7, 6] = "|X4|";
 
             switch (Start)
             {
                 case 1:
-                    array[0,1] = "|OO|";
-                    break;
+                    return 1;
                 case 2:
-                    array[0,3] = "|OO|";
-                    break;
+                    return 3;
                 case 3:
-                    array[0,5] = "|OO|";
-                    break;
+                    return 5;
                 case 4:
-                    array[0,7] = "|OO|";
-                    break;
+                    return 7; 
             }
+            return 0;
         }
+
     }
 }
