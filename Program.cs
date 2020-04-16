447f1ea71d434a6240dae437d89cb5f291f03078
Miguel-Martinho <miguelpm2001@gmail.com>
2020-04-16 19:49:59 +0100

    Adiciona sistema de turnos e comentários em métodos

--- Program.cs
+++ Program.cs
@@ -26,35 +26,48 @@ namespace WolfAndSheep
             board[X3x,X3y] = "|X3|";
             board[X4x,X4y] = "|X4|";
 
-            
-
             // Exemplo de move forwar de uma ovelha 
-            while (playerinput != "exit")
+            do 
             {
-                PrintBoard(board);
-                playerinput = Console.ReadLine();
 
-                if (playerinput == "f")
+                if (turns %2 == 0)
                 {
-                    board[X1x, X1y] = "|__|";
-                    board[X1x - 1, X1y] = "|X1|";
-                    X1x -= 1;
+                    //wolf's turn
+                    Console.WriteLine("Turno do Lobo");
+                    PrintBoard(board);
+                    playerinput = Console.ReadLine();
+                    turns += 1;
                 }
-                if (playerinput == "d")
+                else
                 {
-                    board[X1x, X1y] = "|__|";
-                    board[X1x - 1, X1y + 1] = "|X1|";
-                    X1x -= 1;
-                    X1y += 1;
+                    Console.WriteLine("Turno das Ovelhas");
+                    PrintBoard(board);
+                    playerinput = Console.ReadLine();
+
+                    if (playerinput == "f")
+                    {
+                        board[X1x, X1y] = "|__|";
+                        board[X1x - 1, X1y] = "|X1|";
+                        X1x -= 1;
+                    }
+                    if (playerinput == "d")
+                    {
+                        board[X1x, X1y] = "|__|";
+                        board[X1x - 1, X1y + 1] = "|X1|";
+                        X1x -= 1;
+                        X1y += 1;
+                    }
+                    if (playerinput == "e")
+                    {
+                        board[X1x, X1y] = "|__|";
+                        board[X1x - 1, X1y - 1] = "|X1|";
+                        X1x -= 1;
+                        X1y -= 1;
+                    }
+                    turns += 1;
                 }
-                if (playerinput == "e")
-                {
-                    board[X1x, X1y] = "|__|";
-                    board[X1x - 1, X1y - 1] = "|X1|";
-                    X1x -= 1;
-                    X1y -= 1;
-                }
-            }
+
+            }while (playerinput != "sair");
         }
         private static void Instructions()
         {
@@ -63,7 +76,13 @@ namespace WolfAndSheep
             Console.WriteLine("As peças só se podem mover para quadrados escuros vazios. O lobo vence se chegar a um dos quadrados originais das ovelhas. As ovelhas vencem se bloquearem o lobo de modo a que o mesmo não se consiga mover");
         }
 
-        //Input dos quadrados do tabuleiro no array multidimensional 
+        //Input dos quadrados do tabuleiro no array multidimensional
+        /// <summary>
+        /// Este método "limpa" o board colocando todas as suas posições como vazias para que
+        ///  o sítio de onde as peças se movem não fique vazio 
+        /// </summary>
+        /// <param name="string["></param>
+        /// <param name="array"></param>
         private static void BoardInit(out string[,] array)
         {
             array = new string[8,8];
@@ -77,6 +96,12 @@ namespace WolfAndSheep
         }
 
         // Output do tabuleiro
+        /// <summary>
+        /// Este método dá print da board
+        /// </summary>
+        /// <param name="board">
+        /// A função recebe a board do jogo atual para a imprimir
+        /// </param>
         private static void PrintBoard(string[,] board)
         {
             for (int i = 0; i < 8; i++)
